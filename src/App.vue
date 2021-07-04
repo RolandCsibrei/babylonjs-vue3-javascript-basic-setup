57cedc80f56357989e9fffd546c1dff9efc41b47
Roland Csibrei <roland.csibrei@nascor.tech>
2021-07-04 13:22:44 +0200

    Removal of HelloWorld.js

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <BabylonScene />
+</template>
+
+<script>
+import BabylonScene from "./components/BabylonScene.vue";
+
+export default {
+  name: "App",
+  components: {
+    BabylonScene,
+  },
+};
+</script>
